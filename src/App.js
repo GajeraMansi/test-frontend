0cdcb76297ba1ef4df5eb72ca0d0a1ae1edffe15
MansiGajera <mansi.tameta@gmail.com>
2025-09-19 16:30:51 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  // backend URL (use localhost when testing locally)
+  const backendUrl = "http://localhost:5000/api/todos";
+
+  useEffect(() => {
+    axios.get(backendUrl).then((res) => setTodos(res.data));
+  }, []);
+
+  const addTodo = async () => {
+    if (!text) return;
+    const res = await axios.post(backendUrl, { text });
+    setTodos([...todos, res.data]);
+    setText("");
+  };
+
+  const deleteTodo = async (id) => {
+    await axios.delete(`${backendUrl}/${id}`);
+    setTodos(todos.filter((t) => t._id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>✅ Todo App</h1>
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Add todo..."
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>
+            {todo.text}{" "}
+            <button onClick={() => deleteTodo(todo._id)}>❌</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
